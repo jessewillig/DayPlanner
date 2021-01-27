a42c99042193d2057d2012cdba6bd627673f6b86
Jesse Willig <jessewillig@outlook.com>
2021-01-26 18:26:15 -0800

    added current date to js, fixed links in HTML

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,15 @@
-var
+// variables for text content and save buttons
+var content = $("textarea");
+var savBtns = $(".saveBtn")
+
+// local storage variables
+var listItems = JSON.parse(localStorage.getItem("listItems") || "[]");
+
+// add current date to page
+var currentDate = moment().format("MMMM Do YYYY");
+$("#currentDay").text(currentDate);
+console.log(currentDate);
+
+// time blocks color coded to indicate past, present, future
+
+// save events to local storage
