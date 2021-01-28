de40fedb16727b2466f11c3472c6a213d818c38c
Jesse Willig <jessewillig@outlook.com>
2021-01-27 19:32:50 -0800

    added function to change color based on current hour

--- assets/script.js
+++ assets/script.js
@@ -36,19 +36,37 @@ function first() {
 savBtns.on("click", function () {
     var userInput = $(this).prev().val();
     console.log(userInput);
-    var currentHour = $(this).prev().attr.("data-time");
+    var currentHour = $(this).prev().attr("data-time");
     listItems[currentHour] = userInput;
 
     // local storage
     localStorage.setItem("listItems", JSON.stringify(listItems));
     $("#hour1 .text-input").val(localStorage.getItem("hour1"));
-    $("#hour2 .text-input").val(localStorage.getItem("hour1"));
-    $("#hour3 .text-input").val(localStorage.getItem("hour1"));
-    $("#hour4 .text-input").val(localStorage.getItem("hour1"));
-    $("#hour5 .text-input").val(localStorage.getItem("hour1"));
-    $("#hour6 .text-input").val(localStorage.getItem("hour1"));
-    $("#hour7 .text-input").val(localStorage.getItem("hour1"));
-    $("#hour8 .text-input").val(localStorage.getItem("hour1"));
-    $("#hour9 .text-input").val(localStorage.getItem("hour1"));
-    $("#hour10 .text-input").val(localStorage.getItem("hour1"));
+    $("#hour2 .text-input").val(localStorage.getItem("hour2"));
+    $("#hour3 .text-input").val(localStorage.getItem("hour3"));
+    $("#hour4 .text-input").val(localStorage.getItem("hour4"));
+    $("#hour5 .text-input").val(localStorage.getItem("hour5"));
+    $("#hour6 .text-input").val(localStorage.getItem("hour6"));
+    $("#hour7 .text-input").val(localStorage.getItem("hour7"));
+    $("#hour8 .text-input").val(localStorage.getItem("hour8"));
+    $("#hour9 .text-input").val(localStorage.getItem("hour9"));
+    $("#hour10 .text-input").val(localStorage.getItem("hour10"));
+});
+
+// function to identify colors of rows based on current hour
+$.each((content), function () {
+   var hourData = $(this).attr("data-time");
+   console.log($(this)); 
+   if (parseInt(hourData)===currentTime) {
+       $(this).removeClass("past future");
+       $(this).addClass("current");
+   }
+   if (hourData < currentTime) {
+       $(this).removeClass("current future");
+       $(this).addClass("past");
+   }
+   if (hourData > currentTime) {
+       $(this).removeClass("past current");
+       4(this).addClass("future");
+   }
 });
